7b66e105f51a491bc0987f40ddbf535a2cd480fc
Raed Tabani <raedtabanics@gmail.com>
2025-11-08 10:35:17 +0100

    Keyboard controller Update

--- Assets/_AlienDoctor/Scripts/ComputerOS/KeyboardController.cs
+++ Assets/_AlienDoctor/Scripts/ComputerOS/KeyboardController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class KeyboardController : MonoBehaviour
 {
     [SerializeField] private ScreenDisplay screenDisplay;
-
+    [SerializeField] private LayerMask keyboardLayerMask;
     private void OnEnable()
     {
         KeyboardKey.OnKeyPressed += HandleKeyPressed;
@@ -14,6 +14,32 @@ public class KeyboardController : MonoBehaviour
         KeyboardKey.OnKeyPressed -= HandleKeyPressed;
     }
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleClick();
+        }
+       
+    }
+
+    private void HandleClick()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000f, keyboardLayerMask))
+        {
+            // --- Keyboard Key click ---
+            if (((1 << hit.collider.gameObject.layer) & keyboardLayerMask) != 0)
+            {
+                KeyboardKey key = hit.collider.GetComponentInParent<KeyboardKey>();
+                if (key != null)
+                {
+                    key.Press();
+                    return;
+                }
+            }
+        }
+    }
     private void HandleKeyPressed(string keyValue)
     {
         // Forward key input to the screen display
